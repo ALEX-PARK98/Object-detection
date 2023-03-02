cf43a52dc3e1dd6850af61f6ba774d523ec6fe20
alex <aforalex98@gmail.com>
2023-03-02 15:40:45 -0800

    rm test dir

--- build_own_NN/main.py
+++ build_own_NN/main.py
@@ -5,3 +5,8 @@ import tensorflow as tf
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+def flatten(img_set: np.array) -> np.array:
+
+flatten_x_train = x_train.reshape(60000,784)
+print(flatten_x_train[0])
